6cec2ebdfec2e9bd3c9d806769385f884f313922
NiccolasCente <niccolascente@hotmail.com>
2023-07-19 11:51:31 -0300

    atualizando algumas coisas

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ var typed = new typed("span",{
     backSpeed: 60,
     loop: true
 });
+
+//Cookies//
+var msgCookies = document.getElementById('cookies-msg')
+
+function aceito(){
+    localStorage.lgpd = 'sim'
+    msgCookies.classList.remove('mostrar')
+}
+
+if(localStorage.lgpd == 'sim'){
+    msgCookies.classList.remove('mostrar')
+}else{
+    msgCookies.classList.add('mostrar')
+}
+
